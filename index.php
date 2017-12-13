d1bf1e62e134bf9acde7f5aab1d4161fff5e5593
홍성민 <sugar@hongseongmin-ui-MacBook-Pro.local>
2017-12-13 23:24:39 +0900

    create paging

--- index.php
+++ index.php
@@ -31,6 +31,17 @@
         // include database connection 
         include 'config/database.php';
 
+        // PAGINATION VARIABLES
+        // page is the current page, if there's nothing set, defualt is page 1
+        $page = isset($_GET['page']) ? $_GET['page'] : 1;
+
+        // set records or rows of data per page
+        $records_per_page = 5;
+
+        // calculate for the query LIMIT clause
+        $from_record_num = ($records_per_page * $page) - $records_per_page;
+        
+
         $action = isset($_GET['action']) ? $_GET['action'] : '';
 
         // if it was redirected from delete.php 
@@ -39,8 +50,12 @@
         }
 
         // select all data 
-        $query = "SELECT id, name, description, price FROM products ORDER BY id DESC";
+        $query = "SELECT id, name, description, price FROM products ORDER BY id DESC
+                    LIMIT :from_record_num, :records_per_page";
+
         $stmt = $con->prepare($query);
+        $stmt->bindParam(":from_record_num", $from_record_num, PDO::PARAM_INT);
+        $stmt->bindParam(":records_per_page", $records_per_page, PDO::PARAM_INT);
         $stmt->execute();
 
         // this is how to get number of rows returned
@@ -91,6 +106,22 @@
             }
             // end table 
             echo "</table>";
+
+            // PAGINATION
+            // count total number of rows 
+            $query = "SELECT COUNT(*) as total_rows FROM products";
+            $stmt = $con->prepare($query);
+
+            // execute query 
+            $stmt->execute();
+
+            // get total rows 
+            $row = $stmt->fetch(PDO::FETCH_ASSOC);
+            $total_rows = $row['total_rows'];
+
+            // paginate records 
+            $page_url = "index.php?";
+            include_once "paging.php";
         } else {
             // if no record found
             echo "<div class='alert alert-danger'>No records found.</div>";
